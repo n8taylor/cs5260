e3d866ab275865dfe463d7586faa4c99d971712b
Nate Taylor <nate.taylor@aggiemail.usu.edu>
2023-10-20 18:25:27 -0600

    structuring program and processing input

--- consumer.py
+++ consumer.py
@@ -1,12 +1,56 @@
 import sys
 
-n = len(sys.argv)
-if n != 7:
-    print("""
-Command-line arguments:
-        -rb             Name of bucket that will contain requests
-        -st             Type of request storage [s3, dyamodb]
-        -sn             Name of storage resource
-""")
-for i in range(1, n):
-	print(sys.argv[i], end = " ")
+def processInput():
+    n = len(sys.argv)
+    if n != 7:
+        print("""
+    Command-line arguments:
+            -rb             Name of bucket that will contain requests
+            -st             Type of request storage [s3, dyamodb]
+            -sn             Name of storage resource
+    """)
+        
+    args = {}
+
+    requestBinFlag = False
+    storageTypeFlag = False
+    storageNameFlag = False
+    for i in range(1, n):
+        if requestBinFlag:
+            args["requestBin"] = sys.argv[i]
+            requestBinFlag = False
+        if storageTypeFlag:
+            args["storageType"] = sys.argv[i]
+            storageTypeFlag = False
+        if storageNameFlag:
+            args["storageType"] = sys.argv[i]
+            storageNameFlag = False
+
+        if sys.argv[i] == "-rb":
+            requestBinFlag = True
+            continue
+        if sys.argv[i] == "-st":
+            storageTypeFlag = True
+            continue
+        if sys.argv[i] == "-sn":
+            storageNameFlag = True
+            continue
+    
+    return args
+
+def consume(args):
+    # Loop until some stop condition met
+    #     Try to get request
+    #     If got request
+    #         Process request
+    #     Else
+    #         Wait a while (100ms)
+    # End loop
+    pass
+
+def main():
+    args = processInput()
+    consume(args)
+
+
+main()
