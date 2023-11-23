e0033df87d3b8cdc74e5b25547dcd7b5178ac2fe
Nate Taylor <nate.taylor@aggiemail.usu.edu>
2023-11-22 18:23:40 -0700

    satisfied with lambda request handler and test api events

--- widget-request-handler.py
+++ widget-request-handler.py
@@ -11,24 +11,23 @@ def lambda_handler(event, context):
     try:
         # Parse input data from API Gateway
         print(event)
-        data = json.loads(event['body'])
-        print(data)
 
         # Add data to SQS queue
         response = sqs.send_message(
             QueueUrl=queueUrl,
-            MessageBody=json.dumps(data)
+            MessageBody=json.dumps(event)
         )
 
         # Return success response to API Gateway
         return {
             'statusCode': 200,
-            'body': json.dumps({'status': 'success', 'message': 'Request added to SQS successfully'}),
+            'body': json.dumps({'status': 'success', 'message': 'Request added to SQS successfully'})
         }
     except Exception as e:
         print(e)
-        # Return failure response to API Gateway
+        raise Exception("Error")
         return {
             'statusCode': 500,
             'body': json.dumps({'status': 'error', 'message': f'Error: {str(e)}'}),
+            'received-request': event
         }
